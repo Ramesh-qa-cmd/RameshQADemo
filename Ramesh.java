21bfc4b82e26c449a1a411e0565feb6ffa0066cc
Ramesh-qa-cmd <rameshkumarmunuswamy87@gmail.com>
2025-02-03 12:41:32 +0530

    Update Ramesh.java
    Adding the new funtional

--- Ramesh.java
+++ Ramesh.java
@@ -2,5 +2,7 @@ public class JenkinsExample{
 public static void main(String []args){
 System.out.println("Ramesh");
 System.out.println("Kumar");
+System.out.println("Munuswamy");
+System.out.println("Muni");
 }
 }
